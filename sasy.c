23a38b945691a2804cabb5ceac4fa0f7c8ef05ec
blacpythoz <blacpythoz@gmail.com>
2016-12-09 05:10:14 +0000

    Proper code

--- sasy.c
+++ sasy.c
@@ -1,65 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <xcb/xcb.h>
-#include <signal.h> 
+
+xcb_connection_t *conn;
+xcb_screen_t *screen;
+
+// Counts the number of clients
+uint8_t count ;
+
+// Arrays of clients
+xcb_window_t client[2];
+
+void resize(xcb_window_t win, uint16_t w, uint16_t h)
+{
+    uint32_t values[2] = {w,h};
+    xcb_configure_window(conn,win,XCB_CONFIG_WINDOW_WIDTH
+             | XCB_CONFIG_WINDOW_HEIGHT, values);
+    //xcb_flush(conn);
+} 
+void move(xcb_window_t win, uint16_t x, uint16_t y  )
+{
+    uint32_t values[2] = {x, y};
+    xcb_configure_window(conn,win,XCB_CONFIG_WINDOW_X
+            | XCB_CONFIG_WINDOW_Y,values); 
+    //xcb_flush(conn);
+}
+
+void draw(xcb_window_t win) {
+    xcb_map_window(conn, win);
+    //xcb_flush(conn);
+}
+
+void border(xcb_window_t win, uint16_t width )
+{
+    uint16_t borderColor = 123452;
+    uint32_t borderWidth[1] = { width };
+    xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_BORDER_WIDTH, borderWidth);
+    xcb_change_window_attributes(conn, win, XCB_CW_BORDER_PIXEL, &borderColor);
+    //xcb_flush(conn);
+}
+
+void focus(xcb_window_t win)
+{
+    xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT,win,XCB_CURRENT_TIME);
+    //xcb_flush(conn);
+}
+
+void manageClient(xcb_map_request_event_t *ev)
+{
+    uint16_t sHeight = screen->height_in_pixels;
+    uint16_t sWidth = screen->width_in_pixels;
+    uint8_t x = 20;
+    uint8_t y = 20;
+    uint8_t bWidth = 4;
+    uint16_t height = sHeight-28;
+    uint16_t width = sWidth-28;
+
+    draw(ev->window);
+    if (count == 0)
+    {
+        move(ev->window,x,y);
+        resize(ev->window,width,height);
+        border(ev->window,bWidth);
+        xcb_flush(conn);
+    }
+    else if(count == 1)
+    {
+        width = (sWidth/2) - 20;
+        resize(client[0],width,height);
+        border(client[0],0);
+        //xcb_flush(conn);
+
+        x = x + width + 20;
+        move(ev->window,700,y);
+        resize(ev->window,width,height);
+        border(ev->window,bWidth);
+        xcb_flush(conn);
+    }
+    else 
+    {
+        move(ev->window,10,10);
+        resize(ev->window,width,height);
+        border(ev->window,bWidth);
+        xcb_flush(conn);
+
+    }
+    count++;
+    focus(ev->window);
+}
+
+void eventHandler()
+{
+    xcb_generic_event_t *eve;
+    while(1) 
+    {
+        if((eve=xcb_wait_for_event(conn))) {
+           switch(eve->response_type & ~0x80) {
+                // listen for application /window opening event
+                case XCB_MAP_REQUEST: {
+                    xcb_map_request_event_t *e = (xcb_map_request_event_t *)eve;
+                    client[count] = e->window;
+                    manageClient(e);
+                } break;
+                default: {
+                         printf("To do");
+                 } break;
+           }
+        }
+        free(eve);
+    }
+}
+
 
 int main() 
 {
-    xcb_connection_t *conn;
-    xcb_screen_t *screen;
-    xcb_drawable_t win;
-    xcb_generic_event_t *ev;
-    //Testing
-    int number = 20; // Connection to the X-server on screen 20: put NULL as default
-    conn = xcb_connect(NULL,&number);
-
-    // Get the current screen
+    conn = xcb_connect(NULL,NULL);
     screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-    uint16_t s_width = screen->width_in_pixels;
-    uint16_t s_height = screen->height_in_pixels;
-    uint16_t bord = 3;
-    uint16_t gap = 5;
-    uint16_t x1 = s_width-bord*3;
-    uint16_t y1 = s_height-bord*3;
-
+    count = 0;
+    
     // For managing child windows need to add some mask to root-window
-    uint32_t values[1] = {XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY |
-                        XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT};
+    uint32_t values[1] = {XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY 
+                | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT};
     xcb_change_window_attributes(conn, screen->root, XCB_CW_EVENT_MASK, values);
-
-    // Flush all requests
+    // Force to change effects
     xcb_flush(conn);
 
-    // Loops
-   while(1) { 
-    if((ev=xcb_wait_for_event(conn))) {
-        switch(ev->response_type & ~0x80) {
-            // listen for application /window opening event
-            case XCB_MAP_REQUEST: {
-                xcb_map_request_event_t *e = (xcb_map_request_event_t *)ev;
-                // Then maps the window to the screen
-                xcb_map_window(conn,e->window);
-                uint32_t values[4] = {gap,gap};
-                // resize the x,y
-                xcb_configure_window(conn,e->window,XCB_CONFIG_WINDOW_X|XCB_CONFIG_WINDOW_Y,values); 
-                uint32_t resizeValues[2] = {x1,y1};
-                // resize the height and width
-                xcb_configure_window(conn,e->window, XCB_CONFIG_WINDOW_WIDTH|XCB_CONFIG_WINDOW_HEIGHT,resizeValues);
-                xcb_flush(conn);
-                uint32_t border[1] = { bord };
-                xcb_configure_window(conn, e->window, XCB_CONFIG_WINDOW_BORDER_WIDTH, border);
-                // get the focus
-                xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT,
-                    e->window, XCB_CURRENT_TIME);
-                xcb_flush(conn);
-            } break;
-            default: {
-                printf("Other events");
-            }break;
-        }
-    }
-    // free up the event
-    free(ev);
-    }
-    return 0;
+   eventHandler();
+
+   return 0;
 }
